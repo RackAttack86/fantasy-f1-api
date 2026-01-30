9bea3da169229146b36b1a4ece0ecca9711207b2
Matthew Rackley <rackley86@gmail.com>
2026-01-30 00:59:05 -0500

    Getting started with jwt authentication. Update dependencies in pom, created jwt properties, added jwt config in application.yml

---
+++ src/main/java/com/fantasyf1/fantasy_f1_api/config/JwtProperties.java
@@ -0,0 +1,14 @@
+package com.REDACTED.fantasy_f1_api.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Data
+public class JwtProperties {
+    private String secret;
+    private long expiration;
+}
