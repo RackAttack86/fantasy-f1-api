3c3b74bde720141e60b266cde2af15ab65fe8695
Matthew Rackley <rackley86@gmail.com>
2026-01-30 22:42:14 -0500

    Login dto and auth response dto

---
+++ src/main/java/com/fantasyf1/fantasy_f1_api/auth/dto/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.REDACTED.fantasy_f1_api.auth.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthResponse {
+    
+    private String token;
+    private String tokenType;
+    private long expiresIn;
+}
---
+++ src/main/java/com/fantasyf1/fantasy_f1_api/auth/dto/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.REDACTED.fantasy_f1_api.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
