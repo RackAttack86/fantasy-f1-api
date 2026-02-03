1bf7200dcba65c66744d61c765044f5b54101ae6
Matthew Rackley <rackley86@gmail.com>
2026-02-03 15:47:07 -0500

    Fixed error in TeamController delete endpoint. Void was lowercase and should have been upper case

--- src/main/java/com/fantasyf1/fantasy_f1_api/team/controller/TeamController.java
+++ src/main/java/com/fantasyf1/fantasy_f1_api/team/controller/TeamController.java
@@ -12,16 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
-
-
 
 @RestController
 @RequestMapping("/api/v1/teams")
@@ -57,7 +47,7 @@ public class TeamController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<void> deleteTeam(@PathVariable UUID id) {
+    public ResponseEntity<Void> deleteTeam(@PathVariable UUID id) {
         teamService.deleteTeam(id);
         return ResponseEntity.noContent().build();
     }
