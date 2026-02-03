4c752dcf8ba59490a85ec3cc4477d164c6d40e43
Matthew Rackley <rackley86@gmail.com>
2026-02-03 15:41:51 -0500

    Finished team controller

--- src/main/java/com/fantasyf1/fantasy_f1_api/team/controller/TeamController.java
+++ src/main/java/com/fantasyf1/fantasy_f1_api/team/controller/TeamController.java
@@ -1,5 +1,65 @@
 package com.fantasyf1.fantasy_f1_api.team.controller;
 
+import com.fantasyf1.fantasy_f1_api.team.dto.CreateTeamRequest;
+import com.fantasyf1.fantasy_f1_api.team.dto.TeamResponse;
+import com.fantasyf1.fantasy_f1_api.team.dto.UpdateTeamRequest;
+import com.fantasyf1.fantasy_f1_api.team.service.TeamService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+
+
+@RestController
+@RequestMapping("/api/v1/teams")
+@RequiredArgsConstructor
 public class TeamController {
+    
+    private final TeamService teamService;
+
+    @PostMapping()
+    public ResponseEntity<TeamResponse> createTeam(@Valid @RequestBody CreateTeamRequest request) {
+        TeamResponse response = teamService.createTeam(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TeamResponse> getTeam(@PathVariable UUID id) {
+        TeamResponse response = teamService.getTeamById(id);
+        return ResponseEntity.ok(response);
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<TeamResponse>> getAllTeams() {
+        List<TeamResponse> response = teamService.getAllTeams();
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TeamResponse> updateTeam(
+            @PathVariable UUID id,
+            @Valid @RequestBody UpdateTeamRequest request) {
+        TeamResponse response = teamService.updateTeam(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<void> deleteTeam(@PathVariable UUID id) {
+        teamService.deleteTeam(id);
+        return ResponseEntity.noContent().build();
+    }
     
 }
